a026a7b3d0de5def074571ffd41ce8f89f605f5f
SlobodanStojkovic <sc.slobodanstojkovic@gmail.com>
2021-10-03 18:35:19 +0200

    Header completed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,60 @@
 import "./Header.scss";
-
+import $ from "jquery";
 
 export const Header = () => {
+
+
+    const showMenuOnClick = () => {
+        $(this).toggleClass('active');
+        console.log("Clicked menu");
+        $("#mainListDiv").toggleClass("show_list");
+        $("#mainListDiv").fadeIn();
+    };
+
+
+
     return (
-        <div>Header</div>
+        <>
+            <nav className="nav">
+                <div className="container">
+                    <div className="logoName">
+                        <a href="#">Slobodan Stojkovic</a>
+                    </div>
+                    <div id="mainListDiv" className="main_list">
+                        <ul className="navlinks">
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">My projects</a></li>
+                            <li><a href="#">My skills</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                    <span className="navTrigger" onClick={showMenuOnClick}>
+                        <i></i>
+                        <i></i>
+                        <i></i>
+                    </span>
+                </div>
+            </nav>
+
+            <section className="homeSec">
+
+                <div className="divHeader">
+                    <section className="sec-header">
+                        <h1>Hi, I am Slobodan Stojkovic</h1>
+                        <hr className="col-xs-12 d-none d-sm-block hrHeader" />
+                        <div className="presentationHeader">
+                            <section className="half-header-r d-none d-sm-none d-md-block">
+                                <p className="line1Header">"I'm a Frontend Developer."</p>
+                                <p className="line2Header">"Are you looking for one?"</p>
+                                <p className="line3Header">"Let's work together!"</p>
+                                <p className="line4Header">{`>`}<span className="cursor1Header">_</span></p>
+                            </section>
+                        </div>
+                    </section>
+                </div>
+            </section>
+        </>
     )
 }
+
+
