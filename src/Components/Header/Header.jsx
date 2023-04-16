d7218a705b81f611da436a338fb6fafde19efa7a
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2023-04-16 21:43:50 +0200

    Header mobile view changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -46,7 +46,6 @@ export const Header = () => {
         <div className="divHeader">
           <section className="sec-header">
             <h1>Hi, I am Slobodan Stojkovic.</h1>
-            <hr className="col-xs-12 d-none d-sm-block hrHeader" />
             <div className="presentationHeader">
               <section className="half-header-r d-none d-sm-none d-md-block">
                 <p className="line1Header">"I'm a Frontend Developer."</p>
